ffc603df33954ff49e57f459f5a5953657b8a413
Nikatz <146496118+Nikatz@users.noreply.github.com>
2024-05-29 18:58:23 -0700

    Added and array for the pressure values

--- src/main.cpp
+++ src/main.cpp
@@ -58,52 +58,51 @@ DigitalOutput valve6(OXYGEN_VALVE_PIN);
 
 DigitalOutput Estop_Control(ESTOP_ENABLE_PIN);
 
-// // function declarations for ESC controller :
-// unsigned int readRegister(int);
-// unsigned int writeRegister(int, unsigned int); 
-// long getRPM(); 
-// unsigned int writeThrottle(unsigned int);  
-// void clearRegister(); 
-// byte intToByte(int);
-// void calibrate(); 
-// void startup(); 
-// float getPower(); 
+// function declarations for ESC controller :
+unsigned int readRegister(int);
+unsigned int writeRegister(int, unsigned int); 
+long getRPM(); 
+unsigned int writeThrottle(unsigned int);  
+void clearRegister(); 
+byte intToByte(int);
+void calibrate(); 
+void startup(); 
+float getPower(); 
 
-// unsigned int getRand(int); 
 
-// // defining the tx and rx pins for ESC control
+// defining the tx and rx pins for ESC control
 
-// const int serial1_r = 16;
-// const int serial1_t = 17;
+const int serial1_r = 16;
+const int serial1_t = 17;
 
-// // defining the throttle registers for ESC control (see https://www.castlecreations.com/en/serial-link-010-0121-00page 5)
-// const int rpmRegister = 5;
-// const int voltageRegister = 0; 
-// const int currentRegister = 2; 
-// const int throttleRegister = 128;
-// const int EStopRegister = 130;
-// int deviceID = 0; //+128;
-// // throttle numbers for ESC control
-// const unsigned int throttleNeutral = 32767; 
-// long rpmCurrent; 
-// unsigned int throttleCurr; 
-// unsigned int writeResponse; 
-// float powerCurr; 
+// defining the throttle registers for ESC control (see https://www.castlecreations.com/en/serial-link-010-0121-00page 5)
+const int rpmRegister = 5;
+const int voltageRegister = 0; 
+const int currentRegister = 2; 
+const int throttleRegister = 128;
+const int EStopRegister = 130;
+int deviceID = 0; //+128;
+// throttle numbers for ESC control
+const unsigned int throttleNeutral = 32767; 
+long rpmCurrent; 
+unsigned int throttleCurr; 
+unsigned int writeResponse; 
+float powerCurr; 
 
-// // PID values for ESC control
-// float error;  // errors in rpm 
-// float ierror; 
-// float prevError;  
-// float controller; 
+// PID values for ESC control
+float error;  // errors in rpm 
+float ierror; 
+float prevError;  
+float controller; 
 
-// long setRPM = 40000; 
-// unsigned long currentTime; 
-// unsigned long prevTime; 
-// unsigned long dt; 
+long setRPM = 40000; 
+unsigned long currentTime; 
+unsigned long prevTime; 
+unsigned long dt; 
 
-// float Kp = 0.0003; 
-// float Ki = 0.0005; 
-// float Kd = 0.003; 
+float Kp = 0.0003; 
+float Ki = 0.0005; 
+float Kd = 0.003; 
 
 // * Control Signals
 uint16_t comms = 0x0000;
@@ -129,136 +128,131 @@ bool first_time_in_state = true;
 // //Motor Contol Funtions
 // //////////////////////////////////////////////////////////////////////////////////////
 
-// // put function definitions here:
+// put function definitions here:
 
-// // function reads a register value from the esc 
-// unsigned int readRegister(int registerNum) {
-// // to read rpm, need to send a command packet, then read the response,
-// // then convert the reading to an rpm response
-// // command packet
+// function reads a register value from the esc 
+unsigned int readRegister(int registerNum) {
+// to read rpm, need to send a command packet, then read the response,
+// then convert the reading to an rpm response
+// command packet
 
-//     byte commPack[] = {intToByte(deviceID+128), intToByte(registerNum), 0x00, 0x00, 0x00};
-//     byte checksum = 0x00; 
-//     for (int i = 0; i < 4; i++) {
-//         checksum = checksum - commPack[i];
-//     }
-//     commPack[4] = checksum; 
-//     Serial1.write(commPack, sizeof(commPack)); // commPack is good to write 
+    byte commPack[] = {intToByte(deviceID+128), intToByte(registerNum), 0x00, 0x00, 0x00};
+    byte checksum = 0x00; 
+    for (int i = 0; i < 4; i++) {
+        checksum = checksum - commPack[i];
+    }
+    commPack[4] = checksum; 
+    Serial1.write(commPack, sizeof(commPack)); // commPack is good to write 
 
-//     // reading the response 
-//     byte response[3];// = {0x41, 0x42, 0x43}; 
-//     while(Serial1.available() < 3); 
-//     for (int jj = 0; jj < 3; jj++) { 
-//       byte temp = Serial1.read(); 
-//       //Serial.print(temp); 
-//       //Serial.print("  "); 
-//       response[jj] = temp; 
-//     }
+    // reading the response 
+    byte response[3];// = {0x41, 0x42, 0x43}; 
+    while(Serial1.available() < 3); 
+    for (int jj = 0; jj < 3; jj++) { 
+      byte temp = Serial1.read(); 
+      //Serial.print(temp); 
+      //Serial.print("  "); 
+      response[jj] = temp; 
+    }
 
-//     unsigned int registerVal = (response[0] << 8) | response[1]; 
-//     return registerVal;
-// }
+    unsigned int registerVal = (response[0] << 8) | response[1]; 
+    return registerVal;
+}
 
-// // function writes an integer number to a specified register 
-// unsigned int writeRegister(int registerNum, unsigned int value) {
-//     // convert value and register to binary, keep the same 
-//     byte upper = highByte(value); 
-//     byte lower = lowByte(value); 
+// function writes an integer number to a specified register 
+unsigned int writeRegister(int registerNum, unsigned int value) {
+    // convert value and register to binary, keep the same 
+    byte upper = highByte(value); 
+    byte lower = lowByte(value); 
 
-//     byte commPack[] = {intToByte(deviceID+128), intToByte(registerNum), upper, lower, 0x00};
-//     byte checksum = 0x00; 
-//     for (int i = 0; i < 4; i++) {
-//         checksum = checksum - commPack[i];
-//     }
-//     commPack[4] = checksum; 
-//     Serial1.write(commPack, sizeof(commPack)); // commPack is good to write 
+    byte commPack[] = {intToByte(deviceID+128), intToByte(registerNum), upper, lower, 0x00};
+    byte checksum = 0x00; 
+    for (int i = 0; i < 4; i++) {
+        checksum = checksum - commPack[i];
+    }
+    commPack[4] = checksum; 
+    Serial1.write(commPack, sizeof(commPack)); // commPack is good to write 
 
-//     // reading the response 
-//     byte response[3]; 
-//     while(Serial1.available() < 3); 
-//     for (int jj = 0; jj < 3; jj++) { 
-//         response[jj] = Serial1.read(); 
-//     }
+    // reading the response 
+    byte response[3]; 
+    while(Serial1.available() < 3); 
+    for (int jj = 0; jj < 3; jj++) { 
+        response[jj] = Serial1.read(); 
+    }
 
-//     unsigned int registerVal = (response[0] << 8) | response[1]; 
-//     return registerVal;
+    unsigned int registerVal = (response[0] << 8) | response[1]; 
+    return registerVal;
 
-// }
+}
 
-// // This function specifically read the RPM value of the ESC 
-// long getRPM() {
-//     // getting register value 
-//     unsigned int rpm = readRegister(rpmRegister); 
-//     // scaling 
-//     rpm = long(rpm); 
-//     rpm = rpm*208678.6; 
-//     rpm = rpm/20420; 
-//     return rpm; 
-// }
+// This function specifically read the RPM value of the ESC 
+long getRPM() {
+    // getting register value 
+    unsigned int rpm = readRegister(rpmRegister); 
+    // scaling 
+    rpm = long(rpm); 
+    rpm = rpm*208678.6; 
+    rpm = rpm/20420; 
+    return rpm; 
+}
 
-// float getPower() {
-//   // getting register value 
-//   unsigned int volt = readRegister(voltageRegister); 
-//   unsigned int curr = readRegister(currentRegister); 
+float getPower() {
+  // getting register value 
+  unsigned int volt = readRegister(voltageRegister); 
+  unsigned int curr = readRegister(currentRegister); 
 
-//   float vol = volt/2042.0 * 20.0; 
-//   float cur = curr/2042.0 * 50.0; 
+  float vol = volt/2042.0 * 20.0; 
+  float cur = curr/2042.0 * 50.0; 
 
-//   return vol*cur; 
-// }
+  return vol*cur; 
+}
 
-// unsigned int getRand(int regi) {
-//   unsigned int readVal = readRegister(regi); 
-//   return readVal; 
-// }
+// specifically writes to the throttle register a throttle value 
+// Throttle is 0-65535, with 0 corresponding to 1 ms pulse(full reverse) 
+// 65535 is a 2 ms pulse (full forward), and 32767 is neutral 
+unsigned int writeThrottle(unsigned int throttleVal) {
+    unsigned int response = writeRegister(throttleRegister, throttleVal); 
+    return response; 
+}
 
-// // specifically writes to the throttle register a throttle value 
-// // Throttle is 0-65535, with 0 corresponding to 1 ms pulse(full reverse) 
-// // 65535 is a 2 ms pulse (full forward), and 32767 is neutral 
-// unsigned int writeThrottle(unsigned int throttleVal) {
-//     unsigned int response = writeRegister(throttleRegister, throttleVal); 
-//     return response; 
-// }
+// function clears the register to prepare a new command 
+// not always necessary but recommended (especially at start of a run) 
+void clearRegister() {
+    byte clearer = 0x00; 
+    if (Serial1.available() == 0) {
+        for(int ii = 0; ii < 10; ii++) {
+            Serial1.write(clearer); 
+        }
+    }
+}
 
-// // function clears the register to prepare a new command 
-// // not always necessary but recommended (especially at start of a run) 
-// void clearRegister() {
-//     byte clearer = 0x00; 
-//     if (Serial1.available() == 0) {
-//         for(int ii = 0; ii < 10; ii++) {
-//             Serial1.write(clearer); 
-//         }
-//     }
-// }
+byte intToByte(int val) {
+// to preserve, val must be 255 or less, otherwise truncated
+    return byte(val);
+}
 
-// byte intToByte(int val) {
-// // to preserve, val must be 255 or less, otherwise truncated
-//     return byte(val);
-// }
+void calibrate() {
+  unsigned int store = writeThrottle(65535); 
+  delay(5000); 
+  store = writeThrottle(0); 
+  delay(4000); 
+  store = writeThrottle(throttleNeutral); 
+  delay(7000); 
+}
 
-// void calibrate() {
-//   unsigned int store = writeThrottle(65535); 
-//   delay(5000); 
-//   store = writeThrottle(0); 
-//   delay(4000); 
-//   store = writeThrottle(throttleNeutral); 
-//   delay(7000); 
-// }
+// it seems as though the esc needs a few seconds of neutral throttle to 
+//  initialize the Serial connection. Function sends neutral for 2 seconds
+void startup() {
+  for(int ii = 0; ii < 100; ii++) {
+    rpmCurrent = getRPM(); 
+    clearRegister(); 
 
-// // it seems as though the esc needs a few seconds of neutral throttle to 
-// //  initialize the Serial connection. Function sends neutral for 2 seconds
-// void startup() {
-//   for(int ii = 0; ii < 100; ii++) {
-//     rpmCurrent = getRPM(); 
-//     clearRegister(); 
+    writeResponse = writeThrottle(throttleNeutral);  
+    clearRegister(); 
 
-//     writeResponse = writeThrottle(throttleNeutral);  
-//     clearRegister(); 
-
-//     delay(20); 
-//   }
-// }
-// //////////////////////////////////////////////////////////////////
+    delay(20); 
+  }
+}
+///////////////////////////////////////////////////////////////////////////////////////////////////
 
 void ADC_setup() {
 
@@ -279,15 +273,7 @@ void ADC_setup() {
 }
 
 // constants for ducer 
-const float MIN_CURRENT = 4e-3;
-const float MAX_CURRENT = 20e-3;
-const uint16_t RES_VALVE = 150;
-const float correcting_factor = 1.27;       // correcting factor for the shitty ADC onboard the ESP32
-uint16_t minVolt_420 = (uint16_t)(MIN_CURRENT * RES_VALVE / 3.3 * 8192 * correcting_factor);
-uint16_t maxVolt_420 = constrain((uint16_t)(MAX_CURRENT * RES_VALVE / 3.3 * 8192 * correcting_factor), 0, 0x1fff);
-
-const int MIN_PSI = 0;
-const int MAX_PSI = 833; 
+u_int16_t pressure[8];
 
 //setting up filter
 const double f_s = 100; // Hz
@@ -306,43 +292,43 @@ void get_PSI(){
    // delay(500);
 
     raw1 = ducer_filter(adc1_get_raw(TRANSDUCER1_PIN));
-        pressure1 = _x[0]*raw1 + _y[0];
+        pressure[0] = _x[0]*raw1 + _y[0];
     raw2 = ducer_filter(adc1_get_raw(TRANSDUCER2_PIN));
-        pressure2 = constrain(_x[1]*raw2 + _y[1], 0, 1000);
+        pressure[1] = constrain(_x[1]*raw2 + _y[1], 0, 1000);
     raw3 = ducer_filter(adc1_get_raw(TRANSDUCER3_PIN));
-        pressure3 = constrain(_x[2]*raw3 + _y[2], 0, 1000);
+        pressure[2] = constrain(_x[2]*raw3 + _y[2], 0, 1000);
     raw4 = ducer_filter(adc1_get_raw(TRANSDUCER4_PIN));
-        pressure4 = constrain(_x[3]*raw4 + _y[3], 0, 1000);
+        pressure[3] = constrain(_x[3]*raw4 + _y[3], 0, 1000);
     adc2_get_raw(TRANSDUCER5_PIN, ADC_WIDTH_BIT_13, &raw5);
         raw5 = ducer_filter(raw5);
-        pressure5 = constrain(_x[4]*raw5 + _y[4], 0, 1000);
+        pressure[4] = constrain(_x[4]*raw5 + _y[4], 0, 1000);
     adc2_get_raw(TRANSDUCER5_PIN, ADC_WIDTH_BIT_13, &raw6);
         raw6 = ducer_filter(raw6);
-        pressure6 = constrain(_x[5]*raw6 + _y[5], 0, 1000);
+        pressure[5] = constrain(_x[5]*raw6 + _y[5], 0, 1000);
     adc2_get_raw(TRANSDUCER7_PIN, ADC_WIDTH_BIT_13, &raw7);
         raw7 = ducer_filter(raw7);
-        pressure7 = constrain(_x[6]*raw7 + _y[6], 0, 1000);
+        pressure[6] = constrain(_x[6]*raw7 + _y[6], 0, 1000);
     adc2_get_raw(TRANSDUCER8_PIN, ADC_WIDTH_BIT_13, &raw7);
-        pressure8 = constrain(_x[7]*raw8 + _y[7], 0, 1000);
+        pressure[7] = constrain(_x[7]*raw8 + _y[7], 0, 1000);
 
 	 //Serial.print(", ");
 	 Serial.println(raw1);
 	 Serial.print("PSI: " );
-      Serial.print(pressure1);
+      Serial.print(pressure[0]);
         Serial.print(",");
-    Serial.print(pressure2);
+    Serial.print(pressure[1]);
     //    Serial.print(",");
-    //Serial.print(pressure3);
+    //Serial.print(pressure[2]);
     //    Serial.print(",");
-    //Serial.print(pressure4);
+    //Serial.print(pressure[3]);
     //    Serial.print(",");
-    //Serial.print(pressure5);
+    //Serial.print(pressure[4]);
     //    Serial.print(",");
-    //Serial.print(pressure6);
+    //Serial.print(pressure[5]);
      //   Serial.print(",");
-    //Serial.print(pressure7);
+    //Serial.print(pressure[6]);
       //  Serial.print(",");
-    //Serial.println(pressure8);    
+    //Serial.println(pressure[7]);    
 }
 
 void power_on_state(){
